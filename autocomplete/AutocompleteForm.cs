60a7c9c37e6e7488982b0ba1d1ab1d85a8390bc7
Pavel Egorov <xoposhiy@mail.ru>
2013-10-26 21:25:00 +0600

    initial commit

---
+++ autocomplete/AutocompleteForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace autocomplete
+{
+	public partial class AutocompleteForm : Form
+	{
+		private Autocompleter autocompleter;
+		private long count;
+		private long sumMs;
+
+		public AutocompleteForm()
+		{
+			InitializeComponent();
+		}
+
+		private void inputBox_TextChanged(object sender, EventArgs e)
+		{
+			autocompleteList.Items.Clear();
+
+			// Stopwatch — это полезный инструмент для того, чтобы точно засекать время.
+			// При измерении скорости работы алгоритма нужно использовать именно его.
+			Stopwatch sw = Stopwatch.StartNew();
+			string prefix = inputBox.Text;
+			string[] foundItems = autocompleter.FindByPrefix(prefix, 10);
+			int foundItemsCount = autocompleter.FindCount(prefix);
+			if (foundItems.Length == 0)
+			{
+				string oneItem = autocompleter.FindByPrefix(prefix);
+				if (oneItem != null)
+					foundItems = new[] {oneItem};
+			}
+			sw.Stop();
+			sumMs += sw.ElapsedMilliseconds;
+			count++;
+			statusLabel.Text = string.Format("Found: {0}; Last time: {1} ms; Average time: {2} ms", foundItemsCount, sw.ElapsedMilliseconds, sumMs / count);
+			foreach (string foundItem in foundItems)
+				autocompleteList.Items.Add(foundItem);
+		}
+
+		private void AutocompleteForm_Load(object sender, EventArgs e)
+		{
+			string filename = "..\\..\\..\\words.txt";
+			if (!File.Exists(filename))
+			{
+				MessageBox.Show(
+					string.Format(
+						"Не найден файл со словарем {0}\r\nВозможно вы забыли распаковать файл words.zip",
+						Path.GetFullPath(filename)),
+					"Ошибка");
+				Environment.Exit(1);
+			}
+			autocompleter = new Autocompleter(File.ReadAllLines(filename));
+		}
+	}
+}
---
+++ autocomplete/Autocompleter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace autocomplete
+{
+	// Внимание!
+	// Есть одна распространенная ловушка при сравнении строк: строки можно сравнивать по-разному:
+	// с учетом регистра, без учета, зависеть от кодировки и т.п.
+	// В файле словаря все слова отсортированы мтодом StringComparison.OrdinalIgnoreCase.
+	// Во всех функциях сравнения строк в C# можно передать способ сравнения.
+	class Autocompleter
+	{
+		private readonly string[] items;
+
+		public Autocompleter(string[] loadedItems)
+		{
+			items = loadedItems;
+		}
+
+		// Найти произвольный элемент словаря, начинающийся с prefix.
+		// Ускорьте эту фунцию так, чтобы она работала за O(log(n))
+		public string FindByPrefix(string prefix)
+		{
+			foreach (var item in items)
+			{
+				if (item.StartsWith(prefix, StringComparison.OrdinalIgnoreCase)) return item;
+			}
+			return null;
+		}
+
+		// Найти первые (в порядке следования в файле) 10 (или меньше, если их меньше 10) элементов словаря, 
+		// начинающийся с prefix.
+		// Эта функция должна работать за O(log(n) + count)
+		public string[] FindByPrefix(string prefix, int count)
+		{
+			return new string[0];
+		}
+
+		// Найти количество слов словаря, начинающихся с prefix
+		// Эта функция должна работать за O(log(n))
+		public int FindCount(string prefix)
+		{
+			return -1;
+		}
+	}
+}
